c17ac1f144438f02b9c993987eef6b6ecf427cac
Seth <sepx898508@gmail.com>
2021-12-29 23:33:53 +0800

    feat: add basic settings for devise, create models of users and companies, and add color.orange to tailwind.config

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { 
+    registrations: 'users/registrations',
+  }
+  
+  devise_scope :user do
+    get 'user/profile', to: 'users/registrations#edit'
+  end
+
   root "pages#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
