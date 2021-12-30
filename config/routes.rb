c731c83ab260e085b41bb6e880ba1ebfae3a9d90
ching <ching22h7@gmail.com>
2021-12-30 11:15:16 +0800

    feat: 新增myorder verification頁面

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 
   root "pages#index"
   get 'search', to: 'pages#search'
+  get 'myorder', to: 'pages#myorder'
+  get 'verification', to: 'pages#verification'
   
 end
