125d11e74a75dcbfa04102a137c1a25c593a829b
ching <ching22h7@gmail.com>
2021-12-30 11:28:14 +0800

    feat: 新增channel頁面

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'search', to: 'pages#search'
   get 'myorder', to: 'pages#myorder'
   get 'verification', to: 'pages#verification'
+  get 'channel', to: 'pages#channel'
   
 end
