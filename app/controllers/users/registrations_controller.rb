d41e4fafac296813699b0c336d68a9799384d063
Seth <sepx898508@gmail.com>
2021-12-30 17:43:56 +0800

    feat: add views for users and its permitted params in controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,19 +1,16 @@
-class Users::RegistationsController < Devise::RegistrationsController 
-  before_action :configure_permitted_parameters, only: [:create]
-  before_action :configure_account_update_params, only: [:update]
+class Users::RegistrationsController < Devise::RegistrationsController 
+  before_action :authenticate_user!
+  before_action :permitted_params_for_sign_up, only: [:create]
+  before_action :permitted_params_for_update, only: [:update]
 
   private
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-      user_params.permit(:email, :password, :password_confirmation, :tele, :gender, :role, :username)
-    end
+  def permitted_params_for_sign_up
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :tel, :gender, :role, :name])
   end
 
-  def configure_account_update_params
-    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-      user_params.permit(:email, :password, :password_confirmation, :tele, :gender, :role, :username)
-    end
+  def permitted_params_for_update
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, :tel, :gender, :role, :name])
   end
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
+  # For Devise
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
