d5066d56f759a05d4ad302e914dc0520c66faf54
Seth <sepx898508@gmail.com>
2021-12-30 17:43:56 +0800

    feat: add views for users and its permitted params in controller

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,18 @@ module.exports = {
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
-
+      stone: {
+        50: '#fafaf9',
+        100: '#f5f5f4',
+        200: '#e7e5e4',
+        300: '#d6d3d1',
+        400: '#a8a29e',
+        500: '#78716c',
+        600: '#57534e',
+        700: '#44403c',
+        800: '#292524',
+        900: '#1c1917',
+      },
       black: colors.black,
       white: colors.white,
       gray: colors.coolGray,
@@ -32,6 +43,7 @@ module.exports = {
       indigo: colors.indigo,
       purple: colors.violet,
       pink: colors.pink,
+      lime: colors.lime,
     },
     spacing: {
       px: '1px',
