f6c1680c81538931cd270a18b7eeb0e7e829df6f
Seth <sepx898508@gmail.com>
2022-01-03 11:59:40 +0800

    feat: add intermediary table for users and companies

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,6 +3,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_action :permitted_params_for_sign_up, only: [:create], if: :devise_controller?
   before_action :permitted_params_for_update, only: [:update], if: :devise_controller?
 
+  def create
+    User.role = if User.role.nil?
+  end
+
   def edit
     if resource.save
     else
@@ -12,13 +16,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   private
 
-  def permitted_params_for_sign_up
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :tel, :gender, :role, :name])
-  end
+    def permitted_params_for_sign_up
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :tel, :gender, :role, :name])
+    end
 
-  def permitted_params_for_update
-    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, :tel, :gender, :role, :name])
-  end
+    def permitted_params_for_update
+      devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, :tel, :gender, :role, :name])
+    end
 end
 
 
