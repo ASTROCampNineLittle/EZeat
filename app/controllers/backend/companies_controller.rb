9a7ec22855be869c2e26410ae23e6b6122b31c3c
LaiChu <76055783+LaichuLai@users.noreply.github.com>
2022-01-07 21:46:15 +0800

    原第17行不需要
    company = Company.new(company_params)

--- app/controllers/backend/companies_controller.rb
+++ app/controllers/backend/companies_controller.rb
@@ -13,8 +13,6 @@ class Backend::CompaniesController < ApplicationController
 
   def create
     company = current_user.create_company(company_params)
-
-    company = Company.new(company_params)
     if company.save
       redirect_to backend_company_stores_path(current_user.company.id), notice: '新增公司成功'
     else
