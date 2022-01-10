5f1124da797685a513f6e3ba877900a920dcf010
蔡傑名 <dreamorange830@gmail.com>
2022-01-10 15:38:45 +0800

    修改前台顯示日期從今天開始

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -3,5 +3,6 @@ class DishesController < ApplicationController
     @store = Store.find(params[:store_id])
     @dish = Dish.find(params[:id])
     @open_dates = @dish.open_dates
+    # @availible_dates = @open_dates.joins(open_dates: :offers).pluck(:availible_time)
   end
 end
