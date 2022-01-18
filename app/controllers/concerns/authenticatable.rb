10dc2ea2620702fb9cb2b922fa41b5174b63e729
Laichu <cozzichilli@gmail.com>
2022-01-18 11:34:57 +0800

    feat:修改部署資料

--- app/controllers/concerns/authenticatable.rb
+++
@@ -1,8 +0,0 @@
-# class SignedInChecker
-#   def self.before(controller)
-#     unless controller.send(:signed_in?)
-#       controller.flash[:notice] = I18n.t 'authenticate.signed_in_checker.common'
-#       controller.redirect_to controller.user_session_path
-#     end
-#   end
-# end
