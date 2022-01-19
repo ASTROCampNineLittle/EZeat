66930c56b35ac700b09476567aea21a9d5fb85e4
Seth <sepx898508@gmail.com>
2022-01-19 10:40:21 +0800

    feat: add env settings for production
    * fix: dealing with conflict
    
    * feat: add views for users and its permitted params in controller
    
    * feat: add pages and its controller for routing errors
    
    * feat: add i18n of zh-tw, en, and ja for devise and homepage
    
    * feat: complete all i18n for home page including nav-bar and footer
    
    * feat: add locale namespace before other paths displayed
    
    * feat: finish i18n for error pages and locale switcher
    
    * feat: add sign_in_checker for other actions like creating companies or proceeding to payment
    
    * feat: add intermediary table for users and companies
    
    * feat: add SignInChecker for authenticating users, i18n for multiple lgs users, layout for error pages, fix dropdown menu
    
    * fix: turn off SignInCheck for companies_controller
    
    * feat: member system
    
    * feat: add has_company helper for navbar and css for notice
    
    * chore: add rake file for new users and stores
    
    * feat: replace dropwn-down menu for navbar
    
    * feat: add mailer for devise confirmation and profile page
    
    * chore: add rakefile for creating stores, dishes...... til offers
    
    * feat: add ENV keys for mailer in application.yml
    
    * feat: add controller for after_sign_in_path
    
    * feat: modified dropdown menu of navbar
    
    * feat: add mouseover effect to dropdown menu of navbar
    
    * feat: add google omni log in
    
    * feat: add onchange display to view- show of opendate and translation for user's profile
    
    * feat: add action for after signing in path
    
    * feat: add action for after signing up path
    
    * feat: add around_action to check if has company
    
    * feat: add gem- ransack for implementing search function
    
    * feat: modifying searching function
    
    * feat: add rakefile for creating restaurants
    
    * feat: modified navbar and its logo with link to root path
    
    * feat: add searching functions
    
    * feat: modified flash messages for front-end
    
    * feat: add link to search result
    
    * feat: add rake file for creating dishes for every store
    
    * feat: modify rake file for creating dishes... and etc.
    
    * chore: fix conflict
    
    * feat: add mailgun key and settings as mailer
    
    * feat: feat: add multi-create dishes' function
    
    * feat: 將大量產製方案日期、時間、人數的邏輯與params 接上
    
    * feat: add mailer for after-payment
    
    * feat: add qrcode for my ticket
    
    * fix: settings for mailer in production
    
    * feat: add status for my orders and my tickets for nil and empty

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_16_122705) do
+ActiveRecord::Schema.define(version: 2022_01_18_015946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,6 +96,15 @@ ActiveRecord::Schema.define(version: 2022_01_16_122705) do
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
+  create_table "sessions", force: :cascade do |t|
+    t.string "session_id", null: false
+    t.text "data"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
+  end
+
   create_table "stores", force: :cascade do |t|
     t.bigint "company_id", null: false
     t.string "name"
@@ -111,6 +120,16 @@ ActiveRecord::Schema.define(version: 2022_01_16_122705) do
     t.time "open_time"
     t.time "close_time"
     t.index ["company_id"], name: "index_stores_on_company_id"
+  end
+
+  create_table "tickets", force: :cascade do |t|
+    t.integer "use_status", default: 0
+    t.bigint "order_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["order_id"], name: "index_tickets_on_order_id"
+    t.index ["user_id"], name: "index_tickets_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -151,5 +170,7 @@ ActiveRecord::Schema.define(version: 2022_01_16_122705) do
   add_foreign_key "orders", "stores"
   add_foreign_key "orders", "users"
   add_foreign_key "stores", "companies"
+  add_foreign_key "tickets", "orders"
+  add_foreign_key "tickets", "users"
   add_foreign_key "users", "companies"
 end
