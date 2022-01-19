25be5c466c9bf3c37a939025cee28ebe03de34c6
蔡傑名 <dreamorange830@gmail.com>
2022-01-19 12:33:40 +0800

    Try/撰寫驗證信

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,8 @@
 class UserMailer < ApplicationMailer
   def after_payment(user)
     @user = user
+    @order = @user.orders
     # attachments['票券.jpg'] = File.read('public/Ruby 考試.jpg')
     mail(to: @user.email, subject:"付款成功！")
   end
-
-  # def greeting_mails(user)
-  #   @user = user
-  #   mail(to: @user.email, subject:"歡迎你喔")
-  # end
 end
