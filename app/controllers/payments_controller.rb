46929e169f9a50f913c1bb9db851166bc4adeafe
蔡傑名 <dreamorange830@gmail.com>
2022-01-19 19:29:12 +0800

    fix/deploy_error

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -9,19 +9,16 @@ class PaymentsController < ApplicationController
     @response = Newebpay::Mpgresponse.new(params[:TradeInfo])
     @order = Order.find_by(order_number: @response.order_number)
     @ticket = Order.find_by(order_number: @response.order_number)
-    trade_result_update = @order.update(newebpay_amount: @response.newebpay_amount.to_i,
-                                        newebpay_time: @response.newebpay_time,
-                                        newebpay_card6no: @response.newebpay_card6no)
-    # UserMailer.after_payment(@order.user).deliver_now
 
     if @response.status === "SUCCESS" && @order.order_status === "pending"
+      trade_result_update = @order.update(newebpay_amount: @response.newebpay_amount.to_i,
+                                          newebpay_time: @response.newebpay_time,
+                                          newebpay_card6no: @response.newebpay_card6no)
       @order.succed!
+      @order.update(ticket: "未使用") if @order.succed?
+      # UserMailer.after_payment(@order.user).deliver_now
     else
       @order.fail!
-    end
-
-    if @order.order_status === "succeded"
-      ticket_open = @order.update(ticket: "未使用")
     end
   end
 end
